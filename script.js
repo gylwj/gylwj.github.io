430cab8b8f3ad3a86740cf10fc8103cd78b27b64
gylwj <87127041+gylwj@users.noreply.github.com>
2021-07-26 17:35:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,154 @@
+// declaraction of document.ready() function.
+(function () {
+    var ie = !!(window.attachEvent && !window.opera);
+    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
+    var fn = [];
+    var run = function () {
+        for (var i = 0; i < fn.length; i++) fn[i]();
+    };
+    var d = document;
+    d.ready = function (f) {
+        if (!ie && !wk && d.addEventListener)
+            return d.addEventListener('DOMContentLoaded', f, false);
+        if (fn.push(f) > 1) return;
+        if (ie)
+            (function () {
+                try {
+                    d.documentElement.doScroll('left');
+                    run();
+                } catch (err) {
+                    setTimeout(arguments.callee, 0);
+                }
+            })();
+        else if (wk)
+            var t = setInterval(function () {
+                if (/^(loaded|complete)$/.test(d.readyState))
+                    clearInterval(t), run();
+            }, 0);
+    };
+})();
+
+
+document.ready(
+    // toggleTheme function.
+    // this script shouldn't be changed.
+    function () {
+        var _Blog = window._Blog || {};
+        const currentTheme = window.localStorage && window.localStorage.getItem('theme');
+        const isDark = currentTheme === 'dark';
+        if (isDark) {
+            document.getElementById("switch_default").checked = true;
+            // mobile
+            // document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+        } else {
+            document.getElementById("switch_default").checked = false;
+            // mobile
+            // document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+        }
+        _Blog.toggleTheme = function () {
+            if (isDark) {
+                document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                // mobile
+                // document.getElementById("mobile-toggle-theme").innerText = "· Light"
+            } else {
+                document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                // mobile
+                // document.getElementById("mobile-toggle-theme").innerText = "· Light"
+            }
+            document.getElementsByClassName('toggleBtn')[0].addEventListener('click', () => {
+                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
+                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                } else {
+                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                }
+                window.localStorage &&
+                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light',)
+            })
+            // moblie
+            document.getElementById('mobile-toggle-theme').addEventListener('click', () => {
+                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
+                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                    // mobile
+                    // document.getElementById("mobile-toggle-theme").innerText = "· Light"
+
+                } else {
+                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                    // mobile
+                    // document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+                }
+                window.localStorage &&
+                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light',)
+            })
+        };
+        _Blog.toggleTheme();
+
+        // ready function.
+
+    }
+);
+
+
+console.log(
+      '\n' 
+    + ' %c ꦿ᭄꯭꯭随꯭性꯭এ.' 
+    + ' %c https://hi.sb ' 
+    + '\n', 
+
+    'color: #fadfa3; background: #030307; padding:5px 0; line-height:1.5;', 
+
+    'background: #fadfa3; padding:5px 0; line-height:1.5;'
+
+    );
+
+
+
+
+// 3、统计网站运行时间
+
+// 获取当前日期
+function Today(){
+    let d = new Date();
+    let Y = d.getFullYear();
+    let M = d.getMonth();
+    let D = d.getDate();
+    Today.innerHTML = Y +"年"+ M +"月"+ D +"日"
+}
+ Today();
+
+
+
+
+
+
+
+function secondToDate(second) {
+    if (!second) {
+        return 0;
+    }
+    let time = new Array(0, 0, 0, 0, 0);
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = parseInt(second / 3600);
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[3] = parseInt(second / 60);
+        second %= 60;
+    }
+    if (second > 0) {
+        time[4] = second;
+    }
+    return time;
+}secondToDate();
+
+// function(){
+
+//     $(window).scroll(function(){100<$(this).scrollTop()?$("#back-to-top").fadeIn():$("#back-to-top").fadeOut()}),$("#back-to-top").on("click",function(t){return t.preventDefault(),$("html, body").animate({scrollTop:0},1e3),!1})
+// };
